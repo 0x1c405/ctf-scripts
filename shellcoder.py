49c33a16252a96b7245ff0fb87e1f2e7bc244b29
forw4rdism <105936808+forw4rdism@users.noreply.github.com>
2026-07-11 00:05:48 +0300

    Update shellcoder.py

--- shellcoder.py
+++ shellcoder.py
@@ -5,6 +5,11 @@ from pwn import *
 
 context(os="linux", arch="amd64", log_level="error")
 
+# Requirements:
+# 1. Does not contain variables (entire shellcode must be under '.text' in the assembly code)
+# 2. Does not refer to direct memory addresses (use calls to labels or rip-relative addresses / push to the Stack and use rsp as the address)
+# 3. Does not contain any NULL bytes 00 (use registers that match data size)
+
 file = ELF(sys.argv[1])
 shellcode = file.section(".text")
 print(shellcode.hex())
